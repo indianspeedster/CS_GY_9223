8e86d39e3f7fff7046786a2f4c100144277d892d
indianspeedster <cspandey016@gmail.com>
2023-10-31 18:08:58 +0000

    dockerfile added

--- assignment2/app.py
+++ assignment2/app.py
@@ -4,12 +4,11 @@ from bson.objectid import ObjectId # For ObjectId to work
 from bson.errors import InvalidId # For catching InvalidId exception for ObjectId
 import os
 
-mongodb_host = os.environ.get('MONGO_HOST', 'localhost')
-mongodb_port = int(os.environ.get('MONGO_PORT', '27017'))
-client = MongoClient(mongodb_host, mongodb_port)    #Configure the connection to the database
-db = client.camp2016    #Select the database
-todos = db.todo #Select the collection
-
+mongo_uri = "mongodb://172.17.0.2:27017"  # Replace with your MongoDB URI
+client = MongoClient(mongo_uri)    #Configure the connection to the database
+db = client["mydatabase"]   #Select the database
+todos = db["todos"]#Select the collection
+print(db)
 app = Flask(__name__)
 title = "TODO with Flask"
 heading = "ToDo Reminder"
@@ -19,6 +18,10 @@ def redirect_url():
 	return request.args.get('next') or \
 		request.referrer or \
 		url_for('index')
+
+
+
+
 
 @app.route("/list")
 def lists ():
@@ -120,9 +123,9 @@ def about():
 	return render_template('credits.html',t=title,h=heading)
 
 if __name__ == "__main__":
-	env = os.environ.get('FLASK_ENV', 'development')
-	port = int(os.environ.get('PORT', 8080))
-	debug = False if env == 'production' else True
+	#env = os.environ.get('FLASK_ENV', 'development')
+	#port = int(os.environ.get('PORT', 8080))
+	#debug = False if env == 'production' else True
 	#app.run(debug=True)
-	app.run(port=port, debug=debug)
+	app.run(host="0.0.0.0",port=8200, debug=True)
 	# Careful with the debug mode..
